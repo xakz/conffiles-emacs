e1a1c3d6603828af2d39beb19ef4e3901d5a00fc
Maxime Chatelle <xakz@rxsoft.eu>
2018-01-30 08:35:22 +0100

    Adjusting renaming of ido-ubiquitous to ido-completion-read+

--- init.el
+++ init.el
@@ -498,6 +498,7 @@
   (setq company-idle-delay .3)     ; decrease delay before autocompletion popup shows
   (setq company-echo-delay 0)      ; remove annoying blinking
   (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+  (add-to-list 'company-backends 'company-jedi)
   (global-company-mode 1)
   )
 
@@ -624,7 +625,7 @@
     (ido-vertical-mode 1)
     )
 
-  (use-package ido-ubiquitous
+  (use-package ido-completing-read+
     :config (ido-ubiquitous-mode)
     )
 
